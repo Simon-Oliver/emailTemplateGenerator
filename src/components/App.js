2f6086b16cef3332ede1d718b8c46849eeb46dd5
Simon Stauffer <stauffer.simon@outlook.com>
2019-02-23 15:22:06 +1100

    added file upload

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,47 @@
 import React from 'react';
 
 class App extends React.Component {
+  state = {
+    csvfile: undefined
+  };
+
+  handleChange = event => {
+    this.setState({
+      csvfile: event.target.files[0]
+    });
+  };
+
+  importCSV = () => {
+    const { csvfile } = this.state;
+    const Papa = require('papaparse/papaparse.min.js');
+    Papa.parse(csvfile, {
+      complete: this.updateData,
+      header: true
+    });
+  };
+
+  updateData = result => {
+    const data = result.data;
+    console.log(data);
+  };
+
   render() {
+    console.log(this.state.csvfile);
     return (
-      <div className="ui container" style={{ marginTop: '10px' }}>
-        <div className="ui input">
-          <input type="text" placeholder="Search..." />
-        </div>
+      <div className="App">
+        <h2>Import CSV File!</h2>
+        <input
+          className="csv-input"
+          type="file"
+          ref={input => {
+            this.filesInput = input;
+          }}
+          name="file"
+          placeholder={null}
+          onChange={this.handleChange}
+        />
+        <p />
+        <button onClick={this.importCSV}> Upload now!</button>
       </div>
     );
   }
